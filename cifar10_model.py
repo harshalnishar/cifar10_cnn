d7eb6a9afc961b299944c1167d945ffa7c6cc2ed
Harhsal Nishar <harshalnishar@gmail.com>
2018-07-17 16:00:35 +0530

    Adding my own file and model for cifar10 dataset classification with dataset API

---
+++ cifar10_model.py
@@ -0,0 +1,122 @@
+"""
+File contains neural network model for cifar10 dataset classification
+
+Created on: July 13, 2018
+Author: Harshal
+"""
+
+import tensorflow as tf
+
+def dnn(image):
+    """
+    function defining cifar10 model
+    :param image: input image tensor
+    :return: model output tensor node
+    """
+
+    image = tf.cast(image, tf.float32)
+    image_reshape = tf.reshape(image, [-1, 32, 32, 3])
+
+    conv1 = tf.layers.conv2d(image_reshape, 32, [3, 3], strides = (1, 1), padding = "same",
+                             activation = tf.nn.relu)
+    pool1 = tf.layers.max_pooling2d(conv1, pool_size = [2, 2], strides = 2)
+
+    conv2 = tf.layers.conv2d(pool1, 64, [3, 3], strides = (1, 1), padding = "same",
+                             activation = tf.nn.relu)
+    pool2 = tf.layers.max_pooling2d(conv2, pool_size = [2, 2], strides = 2)
+
+    pool2_flat = tf.reshape(pool2, [-1, 8 * 8 * 64])
+    fc1 = tf.layers.dense(pool2_flat, units=10)
+
+    return fc1
+
+def predict(logits):
+    """
+    function outputs the predicted class based on input logits
+    :param logits: logits tensor
+    :return: returns predicted output with prediction probability
+    """
+    prediction = tf.cast(tf.argmax(logits, axis = 1), tf.int32)
+    probability = tf.reduce_max(tf.nn.softmax(logits), axis = 1)
+    return prediction, probability
+
+def old_evaluate(logits, labels):
+    prediction, _ = predict(logits)
+    match = tf.equal(labels, prediction)
+    accuracy = tf.reduce_mean(tf.cast(match, tf.float32))
+    return accuracy
+
+def evaluate(logits, labels):
+    """
+    function to evaluate the logits output against labels
+    :param logits: logits tensor
+    :param labels: normal (not one hot) labels tensor
+    :return: prediction accuracy
+    """
+    prediction, _ = predict(logits)
+    #match = tf.equal(labels, prediction)
+    #accuracy = tf.reduce_mean(tf.cast(match, tf.float32))
+    accuracy, accuracy_op = tf.metrics.accuracy(labels = labels, predictions = prediction)
+    return accuracy, accuracy_op
+
+def train(logits, labels):
+    """
+    function to train the dnn model for cifar10 training set
+    :param logits: logits tensor
+    :param labels: normal (not one hot) labels tensor
+    :return: trainig operation
+    """
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits = logits, labels = labels))
+
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate= 10e-5)
+    optimizer_step = optimizer.minimize(loss)
+    return loss, optimizer_step
+
+import cifar10_input
+
+if __name__ == "__main__":
+
+    BATCH_SIZE = 256
+    NO_OF_EPOCHS = 1000
+
+    image = tf.placeholder(tf.uint8)
+    label = tf.placeholder(tf.int32)
+
+    dataset_iterator = cifar10_input.input_dataset(image, label, BATCH_SIZE, NO_OF_EPOCHS)
+    data = dataset_iterator.get_next()
+
+    image_queue = data["features"]
+    label_queue = data["label"]
+
+    logits = dnn(image_queue)
+    loss, train_step = train(logits, label_queue)
+    accuracy = evaluate(logits, label_queue)
+
+    path = './dataset/cifar-10-batches-py'
+    filename_list = [(path + '/data_batch_%d' % i) for i in range(1, 6)]
+    cifar10_dataset = cifar10_input.unpickle(filename_list[0])
+    image_in = cifar10_dataset[b'data']
+    label_in = cifar10_dataset[b'labels']
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+        sess.run(dataset_iterator.initializer, feed_dict = {image: image_in, label: label_in})
+        count = 1
+        while True:
+            try:
+                loss_value, _, (_, accuracy_value) = sess.run([loss, train_step, accuracy])
+                if count % 100 == 0:
+                    print("Step: %6d,\tLoss: %8.4f,\tAccuracy: %0.4f" % (count, loss_value, accuracy_value))
+                count += 1
+            except tf.errors.OutOfRangeError:
+                break
+
+        cifar10_dataset = cifar10_input.unpickle(filename_list[1])
+        image_in = cifar10_dataset[b'data']
+        label_in = cifar10_dataset[b'labels']
+
+        sess.run(tf.local_variables_initializer())
+        sess.run(dataset_iterator.initializer, feed_dict = {image: image_in, label: label_in})
+        _, accuracy_value = sess.run(accuracy)
+        print("Accuracy: ", accuracy_value)
